d08c19b0108006b7d4ddb727b77eac7ffe9fb883
Sébastien Werr <werr.sebastien@gmail.com>
2023-11-28 01:59:45 +0100

    base accueil manque responsive et composant recette

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroBannerComponent } from './hero-banner/hero-banner.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeroBannerComponent, MenuComponent, MainSectionComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
