854b6ca68a58b67fb082fb675d4371979c9e12ca
SoaZy <eric.ye@etu.u-paris.fr>
2023-12-04 16:15:13 +0100

    ajout de menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { UserService } from './user/user.service';
 import { ProfilComponent } from './profil/profil.component';
+import { MenuComponent } from './menu/menu.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,10 +10,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, ProfilComponent, NavbarComponent],
+  declarations: [AppComponent, ProfilComponent, MenuComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule,MatListModule,],
   providers: [UserService],
   bootstrap: [AppComponent],
