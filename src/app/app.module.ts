9f500baeb728a1b3ea5516eedf1314e2f46a0e4b
Sébastien Werr <werr.sebastien@gmail.com>
2024-01-05 04:50:48 +0100

    reogarnisation en module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroBannerComponent } from './hero-banner/hero-banner.component';
-import { MenuComponent } from './menu/menu.component';
-import { MainSectionComponent } from './main-section/main-section.component';
-import { FooterComponent } from './footer/footer.component';
-import { RecetteComponent } from './recette/recette.component';
-import { AccueilComponent } from './accueil/accueil.component';
-import { DetailRecetteComponent } from './detail-recette/detail-recette.component';
+// import { HeroBannerComponent } from './page-accueil/hero-banner/hero-banner.component';
+// import { MenuComponent } from './page-accueil/menu/menu.component';
+// import { MainSectionComponent } from './page-accueil/main-section/main-section.component';
+// import { FooterComponent } from './page-accueil/footer/footer.component';
+// import { RecetteComponent } from './recette/recette.component';
+// import { AccueilComponent } from './page-accueil/accueil/accueil.component';
+// import { DetailRecetteComponent } from './page-accueil/detail-recette/detail-recette.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageAccueilModule } from './page-accueil/page-accueil.module';
 
 @NgModule({
-  declarations: [AppComponent, HeroBannerComponent, MenuComponent, MainSectionComponent, FooterComponent, RecetteComponent, AccueilComponent, DetailRecetteComponent, PageNotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, PageNotFoundComponent],
+  imports: [BrowserModule, PageAccueilModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/page-accueil/page-accueil.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AccueilComponent } from './accueil/accueil.component';
+import { DetailRecetteComponent } from './detail-recette/detail-recette.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroBannerComponent } from './hero-banner/hero-banner.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { MenuComponent } from './menu/menu.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const accueilRoutes: Routes = [
+  {path: 'accueil', component: AccueilComponent},
+  {path: 'recette/:id', component: DetailRecetteComponent}
+];
+
+@NgModule({
+  declarations: [
+    AccueilComponent,
+    DetailRecetteComponent,
+    FooterComponent,
+    HeroBannerComponent,
+    MainSectionComponent,
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(accueilRoutes)
+  ]
+})
+export class PageAccueilModule { }
