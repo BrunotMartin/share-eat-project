62914a9a7da6b86217d34ce29c715c71517f2884
BrunotMartin <martinbrunot00@gmail.com>
2024-01-20 15:54:44 +0100

    1st commit backup

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BackendServiceService } from '../backend-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inscription',
@@ -9,13 +10,14 @@ import { BackendServiceService } from '../backend-service.service';
 export class InscriptionComponent {
   credentials = { nom: '', prenom: '', pseudo: '', mail: '', mdp: '' };
 
-  constructor(private backendService: BackendServiceService) { }
+  constructor(private backendService: BackendServiceService, private router: Router) { }
 
   register() {
     this.backendService.registerUser(this.credentials).subscribe(
       response => {
         // Gérer la réponse réussie, par exemple, stocker le jeton d'authentification
         console.log('Inscription réussie:', response);
+        this.router.navigate(['/login']);
       },
       error => {
         // Gérer l'erreur, par exemple, afficher un message d'erreur à l'utilisateur
